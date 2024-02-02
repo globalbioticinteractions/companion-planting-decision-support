ac814f9a8ac08192c4f54e8b3cd3cc1fc1e56950
Patrick Koopmann <p.k.koopmann@vu.nl>
2024-02-02 14:56:51 +0100

    implemented repair under side conditions

--- Companion-Queries/src/main/java/nl/vu/kai/companion/repairs/ClassicalRepairGenerator.java
+++ Companion-Queries/src/main/java/nl/vu/kai/companion/repairs/ClassicalRepairGenerator.java
@@ -13,10 +13,96 @@ import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ClassicalRepairGenerator {
+
+
+    /**
+     * Compute a classical repair with the following constraints:
+     * - only the flexible axioms can be removed
+     * - the given "keepEntailments" have to be kept as entailments
+     */
+    public <T extends OWLAxiom> Optional<Set<T>> computeRepair(
+            OWLOntology ontology,
+            Set<T> flexibleAxioms,
+            Set<OWLAxiom> keepEntailments) {
+
+        OWLOntologyManager owlManager = ontology.getOWLOntologyManager();
+        OWLDataFactory owlFactory = owlManager.getOWLDataFactory();
+
+        OWLAxiom incAxiom = owlFactory.getOWLSubClassOfAxiom(
+                owlFactory.getOWLThing(),
+                owlFactory.getOWLNothing()
+        );
+
+        OWLReasonerFactory reasonerFactory = new ReasonerFactory();
+        OWLReasoner reasoner = reasonerFactory.createReasoner(ontology);
+        ExplanationGeneratorFactory egFactory =
+                new InconsistentOntologyExplanationGeneratorFactory(
+                        reasonerFactory,
+                        owlFactory,
+                        () -> owlManager,
+                        1000);
+
+        OWLFormatter formatter = new OWLFormatter(ontology);
+
+        return innerRepair(ontology,flexibleAxioms,keepEntailments,reasoner, egFactory, formatter, incAxiom);
+    }
+
+    private <T extends OWLAxiom> Optional<Set<T>> innerRepair(
+            OWLOntology ontology,
+            Set<T> flexibleAxioms,
+            Set<OWLAxiom> keepEntailments,
+            OWLReasoner reasoner,
+            ExplanationGeneratorFactory egFactory,
+            OWLFormatter formatter,
+            OWLAxiom incAxiom)  {
+
+        reasoner.flush();
+        if(reasoner.isConsistent()){
+            // the repair is successfull - we can return
+            return Optional.of(Collections.emptySet());
+        }
+        if(!keepEntailments.stream().allMatch(reasoner::isEntailed)){
+            // this branch has failed - we need to backtrack
+            return Optional.empty();
+        }
+
+        ExplanationGenerator<OWLAxiom> explanationGenerator =
+                egFactory.createExplanationGenerator(ontology);
+
+        Optional<Explanation<OWLAxiom>> optExplanation =
+                explanationGenerator.getExplanations(incAxiom,1).stream().findAny();
+
+        assert optExplanation.isPresent() : "Inconsistency couldn't be explained!";
+        Collection<OWLAxiom> candidates = optExplanation.get()
+                .getAxioms();
+
+        Set<T> remove = new HashSet<>();
+
+        boolean success=false;
+        while(!success && !candidates.isEmpty()){
+            OWLAxiom candidate = candidates.iterator().next();
+            candidates.remove(candidate);
+            if(flexibleAxioms.contains(candidate)){
+                Optional<Set<T>> opt =
+                        innerRepair(ontology,flexibleAxioms,keepEntailments,reasoner,egFactory,formatter,incAxiom);
+                if(!opt.isPresent()){
+                    success=true;
+                    remove = opt.get();
+                    remove.add((T)candidate);
+                }
+            }
+        }
+
+        if(!success){
+            return Optional.empty();
+        } else
+            return Optional.of(remove);
+    }
 
     /**
      * Computes a classical repair for the given ontology (assuming it is inconsistent), where it keeps all
