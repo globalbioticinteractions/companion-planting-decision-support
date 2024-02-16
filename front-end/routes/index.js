33f0cd983d2e96a084e09837607f3d30d7062025
Ritten <h.m.roothaert@vu.nl>
2024-02-16 15:23:17 +0100

    added README.md for front-end

--- front-end/routes/index.js
+++ front-end/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var plant_list = 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'KG4S - Companion Planting' });
+  res.render('index', { title: 'KG4S - Companion Planting', plant_list: plant_list});
 });
 
 module.exports = router;
