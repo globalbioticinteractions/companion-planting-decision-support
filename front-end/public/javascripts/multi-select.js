a0f34ce76906c6946ff87ee3e6f73274ce60ab05
Ritten <h.m.roothaert@vu.nl>
2024-02-16 19:24:00 +0100

    very bare-boned front-end is currently working. API integration should be doable

--- front-end/public/javascripts/multi-select.js
+++ front-end/public/javascripts/multi-select.js
@@ -2,7 +2,7 @@
 // import 'script-loader!select2/dist/js/select2.js';
 // import 'script-loader!select2/dist/js/i18n/fr.js';
 
-$( '#multiple-select-field' ).select2( {
+$( '.multiple-select' ).select2( {
   theme: "bootstrap-5",
   width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
   placeholder: $( this ).data( 'placeholder' ),
