3be0fe91cfc4306bdf4c23ea80cd345a86d27df6
pernisch <r.pernisch@vu.nl>
2024-02-20 23:03:25 +0100

    docker compose

--- front-end/public/javascripts/submit-preferences.js
+++ front-end/public/javascripts/submit-preferences.js
@@ -1,4 +1,5 @@
 import parseResult from './parse-result.js'
+import { v2 as compose } from 'docker-compose'
 
 $(document).ready(function(){
     $('#SubmitButton').click(function(){
@@ -21,6 +22,16 @@ function submitPreferences() {
     let message = {'musts': must_ids, 'mays': may_ids}
     
     // TODO: init API call and pass the result to the following function. 
+
+    const service = 'queries'
+    compose.upOne(service, { cwd: path.join(_dirname), log: true})
+    const result = await compose.ps({ cwd: path.join(__dirname), commandOptions: [["--format", "json"]] })
+    result.data.services.forEach((service) => {
+        console.log(service.name, service.command, service.state, service.ports)
+        // state is one of the defined states: paused | restarting | removing | running | dead | created | exited
+    })
+    
+
     parseResult(message)
 
 }
