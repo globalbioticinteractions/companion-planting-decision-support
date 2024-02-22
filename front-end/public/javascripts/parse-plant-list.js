86716827956049cf0cff8e9cbe243ca4cd0e9ae2
Ritten <h.m.roothaert@vu.nl>
2024-02-22 18:14:16 +0100

    fixed plant parsing in slector2 menu

--- front-end/public/javascripts/parse-plant-list.js
+++ front-end/public/javascripts/parse-plant-list.js
@@ -1,35 +1,42 @@
 
 // TODO: Replace this with an API call
-// import data from '../data/taxon_product.js';
-var data = [];
-$.get({
-    url: "http://localhost:8080/getPlants",
-    headers: {'Access-Control-Allow-Origin':'*'}, // <-------- set this
-    dataType: 'json', // // <-------- use JSONP
-    success: function(response){
-        data = response;
-        console.log(data);
-    },
-    // error: function(xhr, status, error) {
-    //     window.alert(xhr.status,status,error);
-        
-    // }
-});
+// import data1 from '../data/taxon_product.js';
+// console.log(data1)
 
-
-addEventListener('DOMContentLoaded', (event) =>
-{
+function parsePlants(data) {
     let multi_selectors = document.querySelectorAll('select.multiple-select');
+    
 
     multi_selectors.forEach(selector => {
+        console.log('populating multi-selector')
         
+        console.log(data)
         
         
         data.forEach(item => {
+            console.log(item)
             let id = item['iri'];
             let text = item['name'].concat(' (', item['scientificName'], ')');
             let option = new Option(text, id, false, false) 
             selector.append(option);
         })
     })
+}
+
+addEventListener('DOMContentLoaded', (event) =>
+{
+    // var data = [];
+    $.get({
+        url: "http://localhost:8080/getPlants",
+        headers: {'Access-Control-Allow-Origin':'*'}, // <-------- set this
+        dataType: 'json', // // <-------- use JSON
+        success: function(response){
+            parsePlants(response);
+            console.log(data);
+        },
+        error: function(xhr, status, error) {
+            window.alert(xhr.status,status,error);
+            
+        }
+    });
 });
