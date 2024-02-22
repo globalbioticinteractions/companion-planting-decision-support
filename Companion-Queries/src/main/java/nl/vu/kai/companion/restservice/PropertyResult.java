cb19660543811b7cbb0ff4d24a815795b5165343
pernisch <r.pernisch@vu.nl>
2024-02-22 18:44:13 +0100

    added text to check response for frontend display

--- Companion-Queries/src/main/java/nl/vu/kai/companion/restservice/PropertyResult.java
+++ Companion-Queries/src/main/java/nl/vu/kai/companion/restservice/PropertyResult.java
@@ -5,18 +5,27 @@ import nl.vu.kai.companion.Configuration.GardenConfigurationProperty;
 public class PropertyResult {
 
 	private final GardenConfigurationProperty property;
-	private final String content;
+	private final String result;
+    private final String text;
 
 	public PropertyResult(GardenConfigurationProperty property, boolean res) {
 		this.property = property;
-		this.content = String.valueOf(res);
+		this.result = String.valueOf(res);
+        if (res) {
+            text = new String("Plants satisfy "+property.name+".");
+        } else
+            text = new String("Plants do not satisfy "+property.name+".");
 	}
 
 	public GardenConfigurationProperty getProperty() {
 		return property;
 	}
 
-	public String getContent() {
-		return content;
+	public String getResult() {
+		return result;
+	}
+
+    public String getText() {
+		return text;
 	}
 }
